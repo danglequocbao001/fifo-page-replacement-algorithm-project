93b898c80f7a60f356d898595fe193c585ac4b1a
Bao Dang <danglequocbao001@gmail.com>
2021-10-26 22:16:52 +0700

    feat: top table and status table

--- FIFO/FIFO.js
+++ FIFO/FIFO.js
@@ -1,10 +1,9 @@
 let reference_string = [1, 2, 3, 4, 2, 1, 5, 6, 2, 1, 2, 3, 7, 6, 3];
-let page_faults = 0,
+let frames = 4;
+let pageFaults = 0,
   counter,
-  pages,
-  frames;
-pages = 15;
-frames = 4;
+  pages = reference_string.length,
+  pagesFaultsArray = ["✘"];
 
 async function PageReplacementFIFOAlgorithm() {
   let tempAnswer = new Array();
@@ -18,14 +17,14 @@ async function PageReplacementFIFOAlgorithm() {
     for (let j = 0; j < frames; j++) {
       if (reference_string[i] == tempAnswer[j]) {
         counter++;
-        page_faults--;
+        pageFaults--;
       }
     }
-    page_faults++;
-    if (page_faults <= frames && counter == 0) {
+    pageFaults++;
+    if (pageFaults <= frames && counter == 0) {
       tempAnswer[i] = reference_string[i];
     } else if (counter == 0) {
-      tempAnswer[(page_faults - 1) % frames] = reference_string[i];
+      tempAnswer[(pageFaults - 1) % frames] = reference_string[i];
     }
     answer = answer.concat(tempAnswer);
   }
@@ -36,7 +35,7 @@ async function handleAnswerVariableTo2DArray(answer) {
   let count = 0;
   let handledAnswer = [];
   tempHandledAnswer = [];
-  for (let i = 0; i < 15; i++) {
+  for (let i = 0; i < pages; i++) {
     for (let j = 0; j < 4; j++) {
       tempHandledAnswer.push(answer[count]);
       count++;
@@ -47,23 +46,83 @@ async function handleAnswerVariableTo2DArray(answer) {
   return handledAnswer;
 }
 
-function tableGenerator(handledAnswer, pages) {
-  const body = document.body,
-    tbl = document.createElement("table");
-  tbl.style.width = "500px";
-  tbl.style.height = "100px";
-  tbl.style.textAlign = "center";
+async function handlePagesFaultsArray(handledAnswer) {
+  for (let i = 0; i < pages; i++) {
+    if (i + 1 != undefined) {
+      if (
+        JSON.stringify(handledAnswer[i]) == JSON.stringify(handledAnswer[i + 1])
+      ) {
+        pagesFaultsArray.push("✓");
+      } else {
+        pagesFaultsArray.push("✘");
+      }
+    }
+  }
+  return pagesFaultsArray;
+}
 
-  for (let i = 0; i < 4; i++) {
-    const tr = tbl.insertRow();
-    for (let j = 0; j < pages; j++) {
+function tableGenerator(handledAnswer, reference_string) {
+  let tempFrames = 0;
+  const body = document.body,
+    mainTable = document.createElement("table");
+  mainTable.style.width = "750px";
+  mainTable.style.height = "100px";
+  mainTable.style.textAlign = "center";
+
+  handlePagesFaultsArray(handledAnswer);
+
+  for (let i = 0; i < 1; i++) {
+    const tr = mainTable.insertRow();
+    for (let j = 0; j <= pages; j++) {
       const td = tr.insertCell();
-      td.appendChild(document.createTextNode(handledAnswer[j][i]));
+      if (j == 0) {
+        td.appendChild(document.createTextNode("Trang"));
+      } else {
+        td.appendChild(document.createTextNode(reference_string[j - 1]));
+      }
+
+      td.style.height = "30px";
+      td.style.color = "#fff";
+      td.style.backgroundColor = "#343a40";
+      td.style.borderColor = "#454d55";
+      td.style.fontWeight = "bold";
+    }
+  }
+
+  for (let i = 0; i < frames; i++) {
+    const tr = mainTable.insertRow();
+    for (let j = 0; j <= pages; j++) {
+      const td = tr.insertCell();
+      if (j == 0) {
+        if (tempFrames <= frames) {
+          tempFrames++;
+          td.appendChild(document.createTextNode("Frame " + tempFrames));
+          td.style.fontWeight = "bold";
+        }
+      } else {
+        td.appendChild(document.createTextNode(handledAnswer[j - 1][i]));
+      }
       td.style.border = "1px solid black";
       td.style.height = "30px";
     }
   }
-  body.appendChild(tbl);
+
+  for (let i = 0; i < 1; i++) {
+    const tr = mainTable.insertRow();
+    for (let j = 0; j <= pages; j++) {
+      const td = tr.insertCell();
+      if (j == 0) {
+        td.appendChild(document.createTextNode("Trạng thái"));
+        td.style.fontWeight = "bold";
+      } else {
+        td.appendChild(document.createTextNode(pagesFaultsArray[j - 1]));
+      }
+      td.style.border = "1px solid black";
+      td.style.height = "30px";
+    }
+  }
+
+  body.appendChild(mainTable);
 }
 
 function interpretCode() {
@@ -72,8 +131,10 @@ function interpretCode() {
     reference_string.length > 0 &&
     PageReplacementFIFOAlgorithm().then((answer) => {
       handleAnswerVariableTo2DArray(answer).then((handledAnswer) => {
-        tableGenerator(handledAnswer, pages);
-        // console.log(page_faults);
+        // console.log(JSON.stringify(handledAnswer[3]) == JSON.stringify(handledAnswer[4]));
+        tableGenerator(handledAnswer, reference_string);
+        // console.log(pageFaults);
+        // console.log(pagesFaultsArray);
       });
     });
 }
